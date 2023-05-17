6dbe7298f4275f519e1d48264d9f68a3aee702d6
Jeff Latham <jeff@fathom5.co>
2023-05-17 08:14:59 -0500

    test

--- a330-hydraulic-system-app/src/index.js
+++ a330-hydraulic-system-app/src/index.js
@@ -61,7 +61,7 @@ let defaultView = {
   // numberChange
   numberChange: function() {
 
-    defaultView.elements.numberTest.firstChild.textContent = 'T';
+    defaultView.elements.numberTest.firstChild.textContent = 'd4';
   },
 };
 

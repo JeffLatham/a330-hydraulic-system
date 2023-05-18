4c8bb51364e608539cea2b4ba49878c54209d21e
Jeff Latham <jeff@fathom5.co>
2023-05-18 11:28:33 -0500

    add button logic

--- a330-hydraulic-system-app/src/index.js
+++ a330-hydraulic-system-app/src/index.js
@@ -13,6 +13,8 @@ import './index.css';
 // Import SVG source files
 import { ReactComponent as HydSystemUI  } from './svg/A330HydSystemUI.svg'
 
+let testValue = 2950;
+
 // getElement does the same thing as "document.getElementById()" except 
 // it's also nice enough to tell you if it can't find the element instead 
 // of just mysteriously breaking the program and leaving you S.O.L. (:
@@ -22,28 +24,10 @@ function getElement(elementID) {
   return element;
 }
 
-// // getElements
-// function getElements(elements) {
-//   const keys = Object.keys(elements);
-//   keys.forEach((key) => {
-//     elements[key] = getElement(key);
-//   });
-// }
-
 // // Default View object
 let hydSystemUI = {
 
-  // // SVG element IDs:
-  // // These are the names of all relevant SVG element "id" attributes.
-  // // This app imports the toolbar.svg file and draws it in the browser.
-  // // This app then modifies the imported SVG elements on the screen 
-  // // based on the element IDs in this object (list). The element "id" 
-  // // names in this list must exactly match the "id" attribute for that 
-  // // element from the SVG file.
-  // elements: {
-
-  //   // testButton: null,
-  // },
+  refreshRate: 10, // Hz
 
   // switches object contains all switch objects
   switches: {
@@ -56,104 +40,153 @@ let hydSystemUI = {
     // based on the switch names listed here.
 
     GreenHydElecPumpOnSwitch: {
-      type: 'on',
+      type: 'latch',
       states: {
-        pushed: false,
-        fault:  false,
-        status: false,
+        Push:   false,
+        Status: false,
+      },
+      statusCriteria: {
+        Push: true,
       },
     },
 
     GreenHydElecPumpOffSwitch: {
-      type: 'off',
+      type: 'latch',
       states: {
-        pushed: false,
-        fault:  false,
-        status: false,
+        Push:   false,
+        Fault:  false,
+        Status: false,
+      },
+      statusCriteria: {
+        Push: true,
       },
     },
 
     GreenHydEng1PumpSwitch: {
-      type: 'off',
+      type: 'latch',
       states: {
-        pushed: false,
-        fault:  false,
-        status: false,
+        Push:   false,
+        Fault:  false,
+        Status: false,
+      },
+      statusCriteria: {
+        Push: true,
+      },
+      faultCriteria: {
+        path: ['engines', 'eng1', 'running'],
+        condition: false,
       },
     },
 
     BlueHydElecPumpOnSwitch: {
-      type: 'on',
+      type: 'latch',
       states: {
-        pushed: false,
-        fault:  false,
-        status: false,
+        Push:   false,
+        Status: false,
+      },
+      statusCriteria: {
+        Push: true,
       },
     },
 
     BlueHydElecPumpOffSwitch: {
-      type: 'off',
+      type: 'latch',
       states: {
-        pushed: false,
-        fault:  false,
-        status: false,
+        Push:   false,
+        Fault:  false,
+        Status: false,
+      },
+      statusCriteria: {
+        Push: true,
       },
     },
 
     BlueHydEng1PumpSwitch: {
-      type: 'off',
+      type: 'latch',
       states: {
-        pushed: false,
-        fault:  false,
-        status: false,
+        Push:   false,
+        Fault:  false,
+        Status: false,
+      },
+      statusCriteria: {
+        Push: true,
+      },
+      faultCriteria: {
+        path: ['engines', 'eng1', 'running'],
+        condition: false,
       },
     },
 
     YellowHydElecPumpOnSwitch: {
-      type: 'on',
+      type: 'latch',
       states: {
-        pushed: false,
-        fault:  false,
-        status: false,
+        Push:   false,
+        Status: false,
+      },
+      statusCriteria: {
+        Push: true,
       },
     },
 
     YellowHydElecPumpOffSwitch: {
-      type: 'off',
+      type: 'latch',
       states: {
-        pushed: false,
-        fault:  false,
-        status: false,
+        Push:   false,
+        Fault:  false,
+        Status: false,
+      },
+      statusCriteria: {
+        Push: true,
       },
     },
 
     YellowHydEng2PumpSwitch: {
-      type: 'off',
+      type: 'latch',
       states: {
-        pushed: false,
-        fault:  false,
-        status: false,
+        Push:   false,
+        Fault:  false,
+        Status: false,
+      },
+      statusCriteria: {
+        Push: true,
+      },
+      faultCriteria: {
+        path: ['engines', 'eng2', 'running'],
+        condition: false,
       },
     },
 
     GreenHydEng2PumpSwitch: {
-      type: 'off',
+      type: 'latch',
       states: {
-        pushed: false,
-        fault:  false,
-        status: false,
+        Push:   false,
+        Fault:  false,
+        Status: false,
+      },
+      statusCriteria: {
+        Push: true,
+      },
+      faultCriteria: {
+        path: ['engines', 'eng2', 'running'],
+        condition: false,
       },
     },
 
     RatSwitch: {
-      type: 'rat',
+      type: 'momentary',
       states: {
-        pushed: false,
-        fault:  false,
-        status: false,
+        Push:   false,
       },
     },
+  },
 
+  covers: {
+
+    RatSwitchCover: {
+      states: {
+        Cover:   false,
+      },
+    },
   },
 
   hydPumps: {
@@ -167,60 +200,147 @@ let hydSystemUI = {
     },
   },
 
-  // init Initializes the Tool Bar
+  hydLoops: {
+
+    greenHydLoop: {
+
+    },
+
+    blueHydLoop: {
+
+    },
+
+    yellowHydLoop: {
+
+    },
+  },
+
+  engines: {
+
+    eng1: {
+      running: false,
+    },
+  
+    eng2: {
+      running: false,
+    },
+  },
+
+  // init Initializes all UI elements
   init: function() {
 
     // getElements(this.elements);
 
-    // Initialize buttons
+    // Initialize Switches
     const allSwitches = Object.keys(this.switches);
     allSwitches.forEach((thisSwitch) => {
 
-      // 
+      // Configure Switch based on initial conditions
       this.updateSwitch(thisSwitch);
         
-      // Attach event
+      // Attach click event
       getElement(thisSwitch+'Button').addEventListener("click", () => this.switchPush(thisSwitch));
     });
+
+    // Initialize Covers
+    const allCovers = Object.keys(this.covers);
+    allCovers.forEach((thisCover) => {
+
+      // Configure Cover based on initial conditions
+      this.coverToggle(thisCover);
+        
+      // Attach click events
+      getElement(thisCover+'Open').addEventListener("click", () => this.coverToggle(thisCover));
+      getElement(thisCover+'Closed').addEventListener("click", () => this.coverToggle(thisCover));
+    });
+
+    // Continuously refresh UI
+    
+    setInterval(() => {
+      this.hsmu();
+    }, 1000/this.refreshRate)
   },
 
   updateSwitch: function(switchName) {
 
-    if(this.switches[switchName].type === 'off' || this.switches[switchName].type === 'on') {
+    const switchStates = Object.keys(this.switches[switchName].states);
+    switchStates.forEach((thisState) => {
 
-      if(this.switches[switchName].states.pushed) {
-        getElement(switchName+'Push').style.display = 'inline';
+      if(this.switches[switchName].states[thisState]) {
+        getElement(switchName+thisState).style.display = 'inline';
       } else {
-        getElement(switchName+'Push').style.display = 'none';
+        getElement(switchName+thisState).style.display = 'none';
       }
-  
-      if(this.switches[switchName].type === 'off') {
-        if(this.switches[switchName].states.fault) {
-          getElement(switchName+'Fault').style.display = 'inline';
-        } else {
-          getElement(switchName+'Fault').style.display = 'none';
-        }
-      }
-  
-      if(this.switches[switchName].states.status) {
-        getElement(switchName+'Status').style.display = 'inline';
-      } else {
-        getElement(switchName+'Status').style.display = 'none';
-      }
-    }
+    });
   },
 
   switchPush: function(switchName) {
 
-    this.switches[switchName].states.pushed = !this.switches[switchName].states.pushed;
+    if(this.switches[switchName].type === 'latch') {
+      this.switches[switchName].states.Push = !this.switches[switchName].states.Push;
+    }
 
-    if(this.switches[switchName].states.pushed) {
-      this.switches[switchName].states.status = true;
-    } else {
-      this.switches[switchName].states.status = false;
+    if(this.switches[switchName].type === 'momentary') {
+
+      this.switches[switchName].states.Push = true;
+
+      setTimeout(() => {
+        this.switches[switchName].states.Push = false;
+        this.updateSwitch(switchName);
+      }, 1000)
     }
 
     this.updateSwitch(switchName);
+  },
+
+  coverToggle: function(coverName) {
+
+    this.covers[coverName].states.Cover = !this.covers[coverName].states.Cover;
+
+    if(this.covers[coverName].states.Cover) {
+      getElement(coverName+'Closed').style.display = 'inline';
+      getElement(coverName+'Open').style.display = 'none';
+    } else {
+      getElement(coverName+'Closed').style.display = 'none';
+      getElement(coverName+'Open').style.display = 'inline';
+    }
+  },
+
+  // hsmu contains all logic for the Hydraulic System Monitoring Unit (HSMU)
+  hsmu: function() {
+
+    // interval test (delete)
+    testValue++;
+    getElement('GreenHydPressureDisplay').firstChild.textContent = testValue;
+
+    // Set switch statuses and faults
+    const allSwitches = Object.keys(this.switches);
+    allSwitches.forEach((thisSwitch) => {
+
+      // Set switch status
+      const allStatusCriteria = Object.keys(this.switches[thisSwitch].statusCriteria);
+      allStatusCriteria.forEach((thisStatusCriteria) => {
+
+        if(this.switches[thisSwitch].statusCriteria[thisStatusCriteria] === this.switches[thisSwitch].states[thisStatusCriteria]) {
+          this.switches[thisSwitch].states.Status = true;
+        } else {
+          this.switches[thisSwitch].states.Status = false;
+        }
+      });
+
+      // Set switch fault
+      if('faultCriteria' in this.switches[thisSwitch]) {
+
+        let path = this.switches[thisSwitch].faultCriteria.path;
+        let condition = this.switches[thisSwitch].faultCriteria.condition;
+        
+        if(this[path[0]][path[1]][[path[2]]] === condition) {
+          this.switches[thisSwitch].states.Fault = true;
+        }
+      }
+      
+      this.updateSwitch(thisSwitch);
+    });
   },
 };
 
